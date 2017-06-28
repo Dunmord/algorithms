7b8ef2434e358a5caba81e765d69ec7e25b11089
dunmord <javierfalconc@gmail.com>
2017-06-27 21:50:53 -0700

    Added support for negative numbers.

--- TwoSum.java
+++ TwoSum.java
@@ -3,17 +3,24 @@ import java.util.Set;
 
 public class TwoSum {
 	
-	public TwoSum(){
-	}
-	
+	/*
+	 Given an array of integers and an integer target, find any 2 numbers in the list that add up to the target.
+	 */
 	public int[] calculate(int[] arrData, int target){
 		int[] twoSum = new int[2];
-		if(target < Integer.MAX_VALUE){
+		if(target > Integer.MIN_VALUE && target < Integer.MAX_VALUE){
 			Set<Integer> o1ArrData = new HashSet<Integer>();
 			
 			for(int a = 0; a < arrData.length; a++){
-				if(arrData[a] <= target){
-					o1ArrData.add(arrData[a]);
+				if(target >= 0){
+					if(arrData[a] <= target){
+						o1ArrData.add(arrData[a]);
+					}
+				}
+				else{
+					if(target <= arrData[a]){
+						o1ArrData.add(arrData[a]);
+					}
 				}
 			}
 			
